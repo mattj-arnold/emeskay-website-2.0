4ed022e83d432bd21d6c22caf306830787b4bb16
mattj-arnold <100128538+mattj-arnold@users.noreply.github.com>
2023-07-24 12:16:11 +0100

    Update top-nav-bar

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+//Top App Bar
+function changeAppBarColorOnScroll() {
+    const appBar = document.getElementById('appBar');
+
+    function handleScroll() {
+      const scrollY = window.scrollY;
+      if (scrollY > 0) {
+        appBar.classList.add('scrolled');
+      } else {
+        appBar.classList.remove('scrolled');
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+  }
+
+  changeAppBarColorOnScroll();
+
+//Modal Navigation Drawer
 function show() {
     document.getElementById('sidebar').classList.toggle('active');
 }
